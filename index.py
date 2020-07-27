c82bc59d0df29dd2e745c99b7b0297f69daebab7
rednirug <sgurinderpreet@gmail.com>
2020-07-26 20:14:35 -0700

    basics test

--- index.py
+++ index.py
@@ -1,1 +1,17 @@
 print('Hello World!')
+
+print(3+3)
+print("3+3")
+print(13 % 2)
+print(9**9)
+
+range(12)
+range(12-40)
+print(range(12-40))
+print(range(5))
+
+
+if (10 < 8):
+    print("10 is less than 8")
+else:
+    print("10 is greater than 8")
